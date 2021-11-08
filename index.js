a5d3d6d6c92604e7ca47945b16d3dfb86b28ce27
Rachel Reilly <r.a.reill18@gmail.com>
2021-11-07 19:33:34 -0600

    prevents empty todo

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ function handleEverything() {
   const form = document.getElementById("add");
   form.onsubmit = addTodo;
   const todos = [
-    { id: 1, content: "rewrew" },
-    { id: 2, content: "rewrew" },
-    { id: 3, content: "rewrew" },
-    { id: 4, content: "rewrew" },
+    { id: 1, content: "Go to store" },
+    { id: 2, content: "Clean floors" },
+    { id: 3, content: "Make dinner" },
+    { id: 4, content: "Walk dog" },
   ];
 
   todos.forEach(makeTodo);
@@ -26,11 +26,19 @@ function handleEverything() {
   function addTodo(e) {
     e.preventDefault();
     const formValue = e?.target?.todo?.value;
-    const id = todos.length + 1;
-    const newTodo = { id, content: formValue };
-    e.target.todo.value = "";
-    todos.unshift(newTodo);
-    makeTodo(newTodo);
+    if (formValue.length > 0 && formValue.length < 50) {
+      const id = todos.length + 1;
+      const newTodo = { id, content: formValue };
+      e.target.todo.value = "";
+      todos.unshift(newTodo);
+      makeTodo(newTodo);
+    }
+  }
+
+  function handleDelete(todo) {
+    //how do I access the id from the button press???
+    const toDelete = document.getElementById(todo.id);
+    document.getElementById("list").removeChild(toDelete);
   }
 }
 
