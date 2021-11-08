8a5b2cf42ef4875d8e6adb50c9547470c547ba63
Rachel Reilly <r.a.reill18@gmail.com>
2021-11-07 19:08:55 -0600

    adds todos at end of list

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
-// make a ul with the todos displayed in LIs
-// make an event listener on the button submit??
-const list = document.createElement("ul");
+function handleEverything() {
+  const form = document.getElementById("add");
+  form.onsubmit = addTodo;
+  const todos = [
+    { id: 1, content: "rewrew" },
+    { id: 2, content: "rewrew" },
+    { id: 3, content: "rewrew" },
+    { id: 4, content: "rewrew" },
+  ];
 
-const todos = [
-  { id: 1, content: "rewrew" },
-  { id: 2, content: "rewrew" },
-  { id: 3, content: "rewrew" },
-  { id: 4, content: "rewrew" },
-];
+  todos.forEach(makeTodo);
 
-todos.forEach(({ content, id }) => {
-  // li contains text and button
-  const elem = document.createElement("li");
-  const elemText = document.createTextNode(content);
-  const elemButton = document.createElement("button");
-  const buttonText = document.createTextNode("Delete");
-  elem.id = `${id}`;
+  function makeTodo({ content, id }) {
+    const elem = document.createElement("li");
+    const elemText = document.createTextNode(content);
+    const elemButton = document.createElement("button");
+    const buttonText = document.createTextNode("Delete");
+    elem.id = `${id}`;
 
-  elemButton.appendChild(buttonText);
-  elem.appendChild(elemText);
-  elem.appendChild(elemButton);
+    elemButton.appendChild(buttonText);
+    elem.appendChild(elemText);
+    elem.appendChild(elemButton);
+    document.getElementById("list").appendChild(elem);
+  }
 
-  document.getElementById("list").appendChild(elem);
-});
-
-function testMe(e) {
-  e.preventDefault();
-  console.log("Im working");
+  function addTodo(e) {
+    e.preventDefault();
+    const formValue = e?.target?.todo?.value;
+    const id = todos.length + 1;
+    const newTodo = { id, content: formValue };
+    e.target.todo.value = "";
+    todos.unshift(newTodo);
+    makeTodo(newTodo);
+  }
 }
 
-document.getElementById("add").onsubmit = testMe;
+handleEverything();
