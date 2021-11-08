8f469bf2c539437e43b6afc9119b66920c930304
Rachel Reilly <r.a.reill18@gmail.com>
2021-11-07 18:38:56 -0600

    dispalying list from map

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("HELLO BROWSER");
+// make a ul with the todos displayed in LIs
+// make an event listener on the button submit??
+const list = document.createElement("ul");
+const todos = ["rewrew", "apples", "bananas", "oranges"];
+
+TEST.forEach((word) => {
+  const elem = document.createElement("li");
+  const elemText = document.createTextNode(word);
+  elem.appendChild(elemText);
+  document.getElementById("list").appendChild(elem);
+});
